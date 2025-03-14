ef8a59767eebb495a10238ffed1bfd92f6b1ff2f
Isaiah Rettig <isaiah.rettig@icloud.com>
2025-03-13 20:45:48 -0400

    3/13 Class
    - Movement
    - Friction
    - Jumping

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,12 +1,61 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 1.0f;
+    [Header("Movement")]
+    public Rigidbody2D rb;
+    public float moveSpeed = 10.0f;
+    private float horizontalMovement;
 
-    void Update()
+    [Header("Jump")]
+    public float jumpPower = 5f;
+    int maxJumps = 2;
+    int jumpsRemaining;
+
+    [Header("GroundCheck")]
+    public Transform groundCheckPos;
+    public Vector2 groundCheckSize = new Vector2(0.5f, 0.05f);
+    public LayerMask groundLayer;
+
+    private void Update()
     {
-        Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
-        transform.Translate(new Vector2(input.x, input.y) * speed * Time.deltaTime);
+        rb.linearVelocity = new Vector2(horizontalMovement * moveSpeed, rb.linearVelocity.y);
+        isGrounded();
+    }
+
+    public void PlayerMove(InputAction.CallbackContext context)
+    {
+        horizontalMovement = context.ReadValue<Vector2>().x;
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (jumpsRemaining > 0) {
+            if (context.performed)
+            {
+                rb.linearVelocityY = jumpPower;
+                jumpsRemaining--;
+            }
+            else if (context.canceled)
+            {
+                rb.linearVelocityY = rb.linearVelocityY * 0.5f;
+                jumpsRemaining--;
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireCube(groundCheckPos.position, groundCheckSize);
+    }
+
+    private void isGrounded()
+    {
+        if (Physics2D.OverlapBox(groundCheckPos.position, groundCheckSize, 0, groundLayer))
+        {
+            jumpsRemaining = maxJumps;
+        }
     }
 }
